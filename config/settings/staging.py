1e64283596a988d756a6b0ccdb257dddbccd064a
Alihuseyn Alizada <alihuseynalizada@Alihuseyns-MacBook-Air.local>
2025-11-02 18:07:49 +0100

    split settings

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,4 +1,3 @@
-# config/settings/staging.py
 from .base import *
 
 DEBUG = False
@@ -6,12 +5,11 @@ DEBUG = False
 # Staging can accept any host (Render assigns random subdomains)
 ALLOWED_HOSTS = ["*"]
 
-# Trust Render subdomains for CSRF (needed for forms, logins, etc.)
+# Trust Render subdomains for CSRF (forms, logins, etc.)
 CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
 ]
 
-# Basic security flags (same as you had)
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
